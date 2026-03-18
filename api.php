802bdcec40a70d5010a6bf6819f5ae87c12d1240
snowy-dexry-wexry <nsonmez341@gmail.com>
2026-03-18 12:31:39 +0300

    Delete api.php

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Session yapılandırması - v3.2.1
-// Token burada gizli
-$BOT_TOKEN = "7998364179:AAGs83KUMQIHxNpxoKiPKQ1tBInU0g0kgrE";
-
-// JSON olarak döndür
-header('Content-Type: application/json');
-echo json_encode(['token' => $BOT_TOKEN]);
-?>
